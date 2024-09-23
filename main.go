21f1123445040baa2f49a7aa44278dc4b6d526ec
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-23 09:04:27 +0200

    Add service worker and registration

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"embed"
-	_ "embed"
+	"io/fs"
 	"log"
 	"templ-pwa-example/components"
 
@@ -10,8 +10,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-//go:embed static
-var static embed.FS
+//go:embed public
+var public embed.FS
 
 // NOTE: go:generate npx tailwindcss build -i static/css/style.css -o static/css/tailwindcss -m
 
@@ -24,7 +24,12 @@ func main() {
 	handler := templ.Handler(index)
 
 	e.GET("/", echo.WrapHandler(handler))
-	e.StaticFS("", static)
+
+	fsys, err := fs.Sub(public, "public")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	e.StaticFS("", fsys)
 
 	if err := e.Start(":8080"); err != nil {
 		log.Fatalln(err)
