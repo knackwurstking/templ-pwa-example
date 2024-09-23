28868d4eaacd9fdfd8f393b9c46b2e4a24991939
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-23 08:14:30 +0200

    embed static and add a note and a todo

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"embed"
+	_ "embed"
 	"log"
 	"templ-pwa-example/components"
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
+
+//go:embed static/*
+var static embed.FS
+
+// NOTE: go:generate npx tailwindcss build -i static/css/style.css -o static/css/tailwindcss -m
 
 func main() {
 	e := echo.New()
@@ -17,6 +24,8 @@ func main() {
 	handler := templ.Handler(index)
 
 	e.GET("/", echo.WrapHandler(handler))
+
+	// TODO: Serve static files
 
 	if err := e.Start(":8080"); err != nil {
 		log.Fatalln(err)
